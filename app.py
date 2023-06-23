4271c987a553fc3b50303710528f770c4eeca9cb
Sksop47 <shubhaman47@gmail.com>
2023-06-23 23:22:09 +0530

    stram lit initalised

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+import subprocess
+import streamlit as st
+import sys
+from PIL import Image
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.set_page_config(page_title="PCB detection", layout="wide",page_icon="assets/icon.png")
+
+def main():
+    st.header("Hello world")
+
+if __name__ == "__main__":
+    main()
